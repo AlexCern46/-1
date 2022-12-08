cf82595487572be4c531be0dedbdd5603b2729cd
Chernyak Alexander <sasha.chernyak.04@mail.ru>
2022-12-08 13:05:23 +0300

    1

--- Курсовая работа № 1/Class1.cs
+++ Курсовая работа № 1/Class1.cs
@@ -8,5 +8,14 @@ namespace Курсовая_работа___1
 {
     internal class Class1
     {
+        public void GetPosition()
+        {
+            string name = Console.ReadLine();
+            string composition = Console.ReadLine();
+            int calories = Convert.ToInt32(Console.ReadLine());
+            double prise = Convert.ToDouble(Console.ReadLine());
+            Menu_position position = new Menu_position(name, composition, calories, prise);
+            Menu.AddPosition(position);
+        }
     }
 }
